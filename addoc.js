cb86c90496459d5c35097f0b239f9e8a973bb329
Vijay S <vijay.sankar@chainsys.com>
2020-03-13 14:13:59 +0530

    updated

--- addoc.js
+++ addoc.js
@@ -27,26 +27,21 @@ function adddoctor() {
     var url = "http://localhost:8089/api/addDoctor?consultingfee=" + conFee + "&doctor_id=" + docId + "&doctor_name=" + docName + "&splzation_id=" + splId;
     console.log(url);
 
-    $.post(url, function (data) {
-        console.log(typeof data);
-        console.log(data);
-        // alert("Doctor Details Added");
-    });
+    $.post(url)
+    .done( function(data){
+        
+   alert(data);
+}).fail( function(err,status){
+    console.log(err);
+    if (err.status == 201) /* jQuery thinks, Status 201 means error, but it doesnt so we have to work around it here */
+    {
+        alert("added Successfully");
+        return;
+    }
+    else{    
+        var msg = err.responseJSON.errorMessage;
+        alert(msg);
+    }
+});
 }
-
-// done( function(){
-   
-// }).fail( function(err,status){
-//     console.log(err);
-//     if (err.status == 201) /* jQuery thinks, Status 201 means error, but it doesnt so we have to work around it here */
-//     {
-//         alert("");
-//         return;
-//     }
-//     else{    
-//         var msg = err.responseJSON.errorMessage;
-//         alert(msg);
-//     }
-// });
-// }
 
